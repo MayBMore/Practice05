2d70fcd8ae45ae7e889e4723189d0ffe3bf1c208
MayBMore <43025864+MayBMore@users.noreply.github.com>
2019-10-15 19:18:47 +0900

    practice05

---
+++ blocking.v
@@ -0,0 +1,34 @@
+module block(	q,
+		d,
+		clk);
+
+	output	q	;
+	input	d	;
+	input	clk	;
+
+	reg	n1	;
+	reg	q	;
+
+	always @(posedge clk ) begin
+		n1 = d; // blocking
+		q = n1; // blocking
+	end
+
+endmodule
+
+module nonblock(	q,
+			d,
+			clk);
+	output	q	;
+	input	d	;
+	input	clk	;
+
+	reg	n1	;
+	reg	q	;
+
+	always @(posedge clk ) begin
+		n1 <= d; // nonblocking
+		q <= n1; // nonblocking
+	end
+
+endmodule
---
+++ bnb.v
@@ -0,0 +1,38 @@
+module tb_bnb	;
+
+reg	d	;
+reg	clk	;
+
+wire	q1	;
+wire	q2	;
+
+initial		clk = 1'b0	;
+always	#(100)	clk = ~clk	;
+
+block		dut_1(	.q	( q1	),
+			.d	( d	),
+			.clk	( clk	));
+
+nonblock	dut_2(	.q	( q2	),
+			.d	( d	),
+			.clk	( clk	));
+
+initial begin
+
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b1	;
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b1	;
+#(50)	{d} = 1'b1	;
+#(50)	{d} = 1'b0	;
+#(50)	{d} = 1'b1	;
+#(50)	{d} = 1'b1	;
+#(50)	{d} = 1'b1	;
+#(50)	$finish;
+
+end
+
+endmodule
